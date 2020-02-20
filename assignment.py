dd2da6fa591edc46b909bf8a74976bf7391027b4
unknown <talha9061@gmail.com>
2020-02-20 12:48:26 +0500

    Assignment for course print helloo statement

---
+++ assignment.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### This Notebook is for a capstone project
+# #### it will import the pandas and numpy libraries and print a helloo world statement and is going to be pushed to the github repository
+
+# In[1]:
+
+
+import pandas as pd 
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
